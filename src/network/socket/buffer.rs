0335c8d4174fc53df0f8905da946eaa1ae18644e
Skynõx <0x536b796ec3b578@proton.me>
2025-11-19 15:45:59 -0500

    Development: removal of a temporary condition

--- src/network/socket/buffer.rs
+++ src/network/socket/buffer.rs
@@ -16,12 +16,14 @@ use tracing::warn;
 /// host system while still remaining configurable via environment variables.
 pub(super) fn tcp_backlog() -> i32 {
     const DEFAULT: usize = 128;
-    #[cfg(target_os = "linux")]
-    const SOMAXCONN_PATH: &str = "/proc/sys/net/core/somaxconn";
-    #[cfg(not(target_os = "linux"))]
-    const SOMAXCONN_PATH: &str = ""; // Unused on non-Linux platforms
 
-    let chosen = env_or_sys("TCP_BACKLOG", SOMAXCONN_PATH).unwrap_or(DEFAULT);
+    let somax_conn_path = if cfg!(target_os = "linux") {
+        Some("/proc/sys/net/core/somaxconn")
+    } else {
+        None
+    };
+
+    let chosen = env_or_sys("TCP_BACKLOG", somax_conn_path).unwrap_or(DEFAULT);
 
     match chosen.try_into() {
         Ok(val) => val,
@@ -37,25 +39,25 @@ pub(super) fn tcp_backlog() -> i32 {
 
 /// Returns the optional TCP receive buffer size (`SO_RCVBUF`) to apply.
 pub(super) fn tcp_recvbuf_size() -> Option<usize> {
-    env_or_sys("TCP_RCVBUF", "/proc/sys/net/core/rmem_default")
+    env_or_sys("TCP_RCVBUF", Some("/proc/sys/net/core/rmem_default"))
 }
 
 /// Returns the optional TCP send buffer size (`SO_SNDBUF`) to apply.
 pub(super) fn tcp_sendbuf_size() -> Option<usize> {
-    env_or_sys("TCP_SNDBUF", "/proc/sys/net/core/wmem_default")
+    env_or_sys("TCP_SNDBUF", Some("/proc/sys/net/core/wmem_default"))
 }
 
 /// Returns the optional UDP receive buffer size (`SO_RCVBUF`) to apply.
 pub(super) fn udp_recvbuf_size() -> Option<usize> {
-    env_or_sys("UDP_RCVBUF", "/proc/sys/net/core/rmem_default")
+    env_or_sys("UDP_RCVBUF", Some("/proc/sys/net/core/rmem_default"))
 }
 
 /// Reads a value from either an environment variable or a sysctl file.
-fn env_or_sys(env_key: &str, sys_path: &str) -> Option<usize> {
+fn env_or_sys(env_key: &str, sys_path: Option<&str>) -> Option<usize> {
     std::env::var(env_key)
         .ok()
         .and_then(|v| v.parse().ok())
-        .or_else(|| read_sys_default(sys_path))
+        .or_else(|| sys_path.and_then(read_sys_default))
 }
 
 /// Reads a kernel default value from the given sysctl file, trimming whitespace and parsing it as `usize`.
